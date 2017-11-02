a4ec72f9e36601b40ad0795e06805660cbccaa34
Shannon Johnson <shannonj498@gmail.com>
2017-11-02 12:25:06 -0600

    Add comments

--- designs.js
+++ designs.js
@@ -1,6 +1,7 @@
 function makeGrid() {
 
   sizePicker.addEventListener('submit', function() {
+    // declare variables
     let width = document.getElementById("input_width").value;
     let height = document.getElementById("input_height").value;
     let table = document.getElementById("pixel_canvas");
@@ -8,24 +9,30 @@ function makeGrid() {
 
     // prevent page refresh
     event.preventDefault();
+    
     // declare blank table
     table.innerHTML = '';
+    
     let newRow;
     let newCell;
-
+    
+    // function to set background color of cell when
     function changeColor() {
       let color = document.getElementById("colorPicker").value;
       this.style.background = color;
     }
-
+    
+    // insert cells based on width and height
     for (var row = 0; row < width; row++) {
       newRow = table.insertRow();
       for (var column = 0; column < height; column++) {
         newCell = newRow.insertCell();
+        // change cell background color on hover
         newCell.onmouseover = changeColor;
       }
     }
   })
 
 }
+// call functions
 makeGrid();
